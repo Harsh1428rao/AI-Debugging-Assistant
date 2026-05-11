be5b9e7c2f1d4756412391a67af961a5dcdb3651
Harsh Rao <hrao1428@gmail.com>
2026-05-11 14:37:42 +0530

    Changes

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -9,7 +9,7 @@ const navItems = [
 
 export default function Layout({ children }) {
   const location = useLocation();
-
+// console.log(location.pathname);
   return (
     <div className="flex min-h-screen bg-terminal-bg font-body">
       {/* Sidebar */}
